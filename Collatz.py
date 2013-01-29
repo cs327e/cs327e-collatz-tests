dc79a28a08a54b2d1c51dd764c57c137c801a5ea
PhillipLin <phillip.lin@utexas.edu>
2013-01-29 14:37:57 -0600

    initial commit

---
+++ Collatz.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+# ---------------------------
+# projects/collatz/Collatz.py
+# Copyright (C) 2013
+# Glenn P. Downing
+# ---------------------------
+
+# ------------
+# collatz_read
+# ------------
+
+def collatz_read (r, a) :
+    """
+    reads two ints into a[0] and a[1]
+    r is a  reader
+    a is an array of int
+    return true if that succeeds, false otherwise
+    """
+    s = r.readline()
+    if s == "" :
+        return False
+    l = s.split()
+    a[0] = int(l[0])
+    a[1] = int(l[1])
+    assert a[0] > 0
+    assert a[1] > 0
+    return True
+
+# ------------
+# collatz_eval
+# ------------
+
+def collatz_eval (i, j) :
+    """
+    i is the beginning of the range, inclusive
+    j is the end       of the range, inclusive
+    return the max cycle length in the range [i, j]
+    """
+    assert i > 0
+    assert j > 0
+    # <your code>
+
+    cl = [0]*j
+    
+    maxCycle = 1
+    
+    for x in range(i,j+1) :
+        index = x
+        cycle = 1
+        while x != 1 :
+            if x % 2 == 0 : 
+                x = x/2
+              
+                if x <= j :
+                    if cl[x-1] != 0 :
+                        cycle = cycle + cl[x-1] - 1
+                        x = 1
+                cycle += 1                        
+ 
+            else :
+                x = 3*x + 1
+                cycle+=1
+
+        cl[index-1] = cycle
+        
+        if cycle > maxCycle :
+            maxCycle = cycle
+    v = maxCycle
+    assert v > 0
+    return v
+
+
+# -------------
+# collatz_print
+# -------------
+
+def collatz_print (w, i, j, v) :
+    """
+    prints the values of i, j, and v
+    w is a writer
+    i is the beginning of the range, inclusive
+    j is the end       of the range, inclusive
+    v is the max cycle length
+    """
+    w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
+
+# -------------
+# collatz_solve
+# -------------
+
+def collatz_solve (r, w) :
+    """
+    read, eval, print loop
+    r is a reader
+    w is a writer
+    """
+    a = [0, 0]
+    while collatz_read(r, a) :
+        if a[0] < a[1] :
+            v = collatz_eval(a[0], a[1])
+            collatz_print(w, a[0], a[1], v)
+        else :
+            v = collatz_eval(a[1],a[0])
+            collatz_print(w,a[0],a[1],v)
